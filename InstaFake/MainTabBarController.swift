6f27de651053820a19369e68b2b148a571f36f46
Aleksey Shapoval <alex@negativeimprint.com>
2019-11-20 12:15:18 -0500

    Added MainTabBarController

---
+++ InstaFake/MainTabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  MainTabBarController.swift
+//  InstaFake
+//
+//  Created by Aleksey Shapoval on 11/20/19.
+//  Copyright © 2019 VoVa LLC. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
