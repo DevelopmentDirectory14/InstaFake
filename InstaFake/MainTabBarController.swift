b2f2096ae7a18e6a1723474a0171b6bfc779aa2b
Aleksey Shapoval <alex@negativeimprint.com>
2019-11-20 12:20:09 -0500

    Setup redVC

--- InstaFake/MainTabBarController.swift
+++ InstaFake/MainTabBarController.swift
@@ -12,5 +12,12 @@ class MainTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
+        
+        let redVC = UIViewController()
+        redVC.view.backgroundColor = .red
+        
+        let navController = UINavigationController(rootViewController: redVC)
+        
+        viewControllers = [navController]
     }
 }
---
+++ InstaFake/UserProfileController.swift
@@ -0,0 +1,11 @@
+//
+//  UserProfileController.swift
+//  InstaFake
+//
+//  Created by Aleksey Shapoval on 11/20/19.
+//  Copyright © 2019 VoVa LLC. All rights reserved.
+//
+
+import UIKit
+
+
