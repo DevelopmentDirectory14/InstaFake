624244150fa2e69ca5f83f7802051283645bcd32
Aleksey Shapoval <alex@negativeimprint.com>
2019-11-20 16:39:43 -0500

    Refactored code, with the setupProfileImage method

--- InstaFake/UserProfile/UserProfileHeader.swift
+++ InstaFake/UserProfile/UserProfileHeader.swift
@@ -26,57 +26,40 @@ class UserProfileHeader: UICollectionViewCell {
         profileImageView.anchor(top: topAnchor, left: self.leftAnchor, bottom: nil, right: nil, paddingTop: 12, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: 80, height: 80)
         profileImageView.layer.cornerRadius = 80 / 2
         profileImageView.clipsToBounds = true
-        
-        setupProfileImage()
+
     }
     
     var user: User? {
         didSet {
-            guard let profileImageUrl = user?.profileImageUrl else { return }
-            
-            guard let url = URL(string: profileImageUrl) else { return }
-            
-            URLSession.shared.dataTask(with: url) { (data, response, err) in
-                //check for the error, then construct the image using data
-                
-                if let err = err {
-                    print("Failed to fetch profile image:", err)
-                    return
-                }
-                
-                print(data)
-                
-                guard let data = data else { return }
-                let image = UIImage(data: data)
-                
-                //need to get back onto the main UI thread
-                DispatchQueue.main.async {
-                    self.profileImageView.image = image
-                }
-                
-            }.resume()
+            setupProfileImage()
         }
     }
     
     fileprivate func setupProfileImage() {
         
-        //copied Firebase fetch code
-        guard let uid = Auth.auth().currentUser?.uid else { return }
+        guard let profileImageUrl = user?.profileImageUrl else { return }
         
-        Database.database().reference().child("users").child(uid).observeSingleEvent(of: .value, with: { (snapshot) in
-            print(snapshot.value ?? "")
-            
-            guard let dictionary = snapshot.value as? [String : Any] else { return }
-            
-            guard let profileImageUrl = dictionary["profileImageUrl"] as? String else { return }
-            
-            
-            
-        }) { (err) in
-            print("Failed to fetch user:", err)
-        }
+        guard let url = URL(string: profileImageUrl) else { return }
         
-        
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            //check for the error, then construct the image using data
+            
+            if let err = err {
+                print("Failed to fetch profile image:", err)
+                return
+            }
+            
+            print(data)
+            
+            guard let data = data else { return }
+            let image = UIImage(data: data)
+            
+            //need to get back onto the main UI thread
+            DispatchQueue.main.async {
+                self.profileImageView.image = image
+            }
+            
+        }.resume()
     }
     
     required init?(coder: NSCoder) {
