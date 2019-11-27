f945ee750b0e290816d2aebfe7befa0f9a1d49ef
Aleksey Shapoval <alex@negativeimprint.com>
2019-11-27 16:55:08 -0500

    add image into share view

--- InstaFake/AddPhoto/SharePhotoController.swift
+++ InstaFake/AddPhoto/SharePhotoController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class SharePhotoController: UIViewController {
+    
+    var selectedImage: UIImage? {
+        didSet {
+            self.imageView.image = selectedImage
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.rgb(red: 240, green: 240, blue: 240)
@@ -22,6 +29,7 @@ class SharePhotoController: UIViewController {
        let iv = UIImageView()
         iv.backgroundColor = .red
         iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
         return iv
     }()
     
@@ -30,7 +38,7 @@ class SharePhotoController: UIViewController {
         containerView.backgroundColor = .white
         
         view.addSubview(containerView)
-        containerView.anchor(top: topLayoutGuide.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 100)
+        containerView.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 100)
         
         containerView.addSubview(imageView)
         imageView.anchor(top: containerView.topAnchor, left: containerView.leftAnchor, bottom: containerView.bottomAnchor, right: nil, paddingTop: 8, paddingLeft: 8, paddingBottom: 8, paddingRight: 8, width: 84, height: 0)
