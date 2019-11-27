210730a9a31fca81fd88c2a9ebb24b3814d19e8d
Aleksey Shapoval <alex@negativeimprint.com>
2019-11-27 15:43:45 -0500

    setup image share box

--- InstaFake/AddPhoto/SharePhotoController.swift
+++ InstaFake/AddPhoto/SharePhotoController.swift
@@ -11,7 +11,23 @@ import UIKit
 class SharePhotoController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = UIColor.rgb(red: 240, green: 240, blue: 240)
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Share", style: .plain, target: self, action: #selector(handleShare))
+        
+        setupImageAndTextViews()
+    }
+    
+    fileprivate func setupImageAndTextViews() {
+        let containerView = UIView()
+        containerView.backgroundColor = .white
+        
+        view.addSubview(containerView)
+        containerView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 100)
+    }
+    
+    @objc func handleShare() {
+        print("Sharing photo")
     }
     
     override var prefersStatusBarHidden: Bool {
