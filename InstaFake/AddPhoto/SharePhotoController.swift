8de207515585012b8416af0685c26492207e3e8a
Aleksey Shapoval <alex@negativeimprint.com>
2019-12-10 18:07:28 -0500

    Successfully uploaded shared photo and got full URL

--- InstaFake/AddPhoto/SharePhotoController.swift
+++ InstaFake/AddPhoto/SharePhotoController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class SharePhotoController: UIViewController {
     
@@ -33,6 +34,12 @@ class SharePhotoController: UIViewController {
         return iv
     }()
     
+    let textView: UITextView = {
+        let tv = UITextView()
+        tv.font = UIFont.systemFont(ofSize: 14)
+        return tv
+    }()
+    
     fileprivate func setupImageAndTextViews() {
         let containerView = UIView()
         containerView.backgroundColor = .white
@@ -42,10 +49,44 @@ class SharePhotoController: UIViewController {
         
         containerView.addSubview(imageView)
         imageView.anchor(top: containerView.topAnchor, left: containerView.leftAnchor, bottom: containerView.bottomAnchor, right: nil, paddingTop: 8, paddingLeft: 8, paddingBottom: 8, paddingRight: 8, width: 84, height: 0)
+        
+        containerView.addSubview(textView)
+        textView.anchor(top: containerView.topAnchor, left: imageView.rightAnchor, bottom: containerView.bottomAnchor, right: containerView.rightAnchor, paddingTop: 0, paddingLeft: 4, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
     }
     
     @objc func handleShare() {
-        print("Sharing photo")
+        guard let image = selectedImage else { return }
+        
+        guard let uploadData = image.jpegData(compressionQuality: 0.5) else { return }
+        
+        let filename =  NSUUID().uuidString
+        
+        let storageRef = Storage.storage().reference().child(filename)
+        
+        storageRef.putData(uploadData, metadata: nil) { (metadata, err) in
+            
+            if let err = err {
+                print("Failed to upload shared image:", err)
+                return
+            }
+            
+            storageRef.downloadURL { (downloadURL, err) in
+                
+                if let err = err {
+                    print("Failed to get shared image downloadURL:", err)
+                    return
+                }
+                
+                guard let sharedImageURL = downloadURL?.absoluteString else { return }
+                
+                print("Successfully uploaded shared image:", sharedImageURL)
+                
+            }
+            
+        }
+        
+        
     }
     
     override var prefersStatusBarHidden: Bool {
