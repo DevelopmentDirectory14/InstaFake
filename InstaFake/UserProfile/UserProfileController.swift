22f86c478370e3eb265023bc9b3dd3e6ceb125a1
Aleksey Shapoval <alex@negativeimprint.com>
2019-12-11 11:52:07 -0500

    Create custom user profile photo cell to use in the user profile controller

--- InstaFake/UserProfile/UserProfileController.swift
+++ InstaFake/UserProfile/UserProfileController.swift
@@ -24,12 +24,14 @@ class UserProfileController: UICollectionViewController, UICollectionViewDelegat
         
         collectionView?.register(UserProfileHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "headerId")
         
-        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView?.register(UserProfilePhotoCell.self, forCellWithReuseIdentifier: cellId)
         
         setupLogOutButton()
         
         fetchPosts()
     }
+    
+    var posts = [Post]()
     
     fileprivate func fetchPosts() {
         guard let uid = Auth.auth().currentUser?.uid else { return }
@@ -46,8 +48,10 @@ class UserProfileController: UICollectionViewController, UICollectionViewDelegat
                 guard let dictionary = value as? [String: Any] else { return }
                 
                 let post = Post(dictionary: dictionary)
-                print(post.sharedImageURL)
+                self.posts.append(post)
             }
+            
+            self.collectionView?.reloadData()
             
         }) { (err) in
             print("Failed to fetch posts:", err)
@@ -82,7 +86,7 @@ class UserProfileController: UICollectionViewController, UICollectionViewDelegat
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 7
+        return posts.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
---
+++ InstaFake/UserProfile/UserProfilePhotoCell.swift
@@ -0,0 +1,32 @@
+//
+//  UserProfilePhotoCell.swift
+//  InstaFake
+//
+//  Created by Aleksey Shapoval on 12/11/19.
+//  Copyright © 2019 VoVa LLC. All rights reserved.
+//
+
+import UIKit
+
+class UserProfilePhotoCell: UICollectionViewCell {
+    
+    let photoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.backgroundColor = .red
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(photoImageView)
+        photoImageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
