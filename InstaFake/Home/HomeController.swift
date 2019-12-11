6ac6e48af8ca9db1e529a8cfcc3eea50d3cf9d2a
Aleksey Shapoval <alex@negativeimprint.com>
2019-12-11 15:44:46 -0500

    Added cells to the HomeController

--- InstaFake/Home/HomeController.swift
+++ InstaFake/Home/HomeController.swift
@@ -10,8 +10,25 @@ import UIKit
 
 class HomeController: UICollectionViewController {
     
+    let cellId = "cellId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView?.backgroundColor = .red
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        
+        cell.backgroundColor = .yellow
+        return cell
+    }
+    
 }
