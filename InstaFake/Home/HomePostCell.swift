3229311a68788a245bd6f642fd001108e25d6524
Aleksey Shapoval <alex@negativeimprint.com>
2019-12-11 17:58:25 -0500

    add action buttons

--- InstaFake/Home/HomePostCell.swift
+++ InstaFake/Home/HomePostCell.swift
@@ -47,8 +47,28 @@ class HomePostCell: UICollectionViewCell {
         return button
     }()
     
+    let likeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "like_unselected")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        return button
+    }()
+    
+    let commentButton: UIButton = {
+           let button = UIButton(type: .system)
+           button.setImage(UIImage(named: "comment")?.withRenderingMode(.alwaysOriginal), for: .normal)
+           return button
+       }()
+    
+    let sendMessageButton: UIButton = {
+           let button = UIButton(type: .system)
+           button.setImage(UIImage(named: "send2")?.withRenderingMode(.alwaysOriginal), for: .normal)
+           return button
+       }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        backgroundColor = .gray
         
         addSubview(userProfileImageView)
         addSubview(usernameLabel)
@@ -62,7 +82,20 @@ class HomePostCell: UICollectionViewCell {
         
         optionsButton.anchor(top: topAnchor, left: nil, bottom: photoImageView.topAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 44, height: 0)
         
-        photoImageView.anchor(top: userProfileImageView.bottomAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 8, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        photoImageView.anchor(top: userProfileImageView.bottomAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, paddingTop: 8, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
+        photoImageView.heightAnchor.constraint(equalTo: widthAnchor, multiplier: 1).isActive = true
+        
+        setupActionButtons()
+    }
+    
+    fileprivate func setupActionButtons() {
+        let stackView = UIStackView(arrangedSubviews: [likeButton, commentButton, sendMessageButton])
+        
+        stackView.distribution = .fillEqually
+        
+        addSubview(stackView)
+        stackView.anchor(top: photoImageView.bottomAnchor, left: leftAnchor, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 8, paddingBottom: 0, paddingRight: 0, width: 150 , height: 50)
     }
     
     required init?(coder: NSCoder) {
