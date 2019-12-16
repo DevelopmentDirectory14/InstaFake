d33263297cf302d1d3ce5f6fd2747467e1cabfa8
Aleksey Shapoval <alex@negativeimprint.com>
2019-12-16 12:17:19 -0500

    calculate time since for posts and display in a user friendly way with a Date extension

--- InstaFake/Home/HomeController.swift
+++ InstaFake/Home/HomeController.swift
@@ -72,7 +72,11 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
                          
                           self.posts.append(post)
                       }
-                      
+                    
+                    self.posts.sort { (p1, p2) -> Bool in
+                        return p1.creationDate.compare(p2.creationDate) == .orderedDescending
+                    }
+                    
                       self.collectionView?.reloadData()
                       
                   }) { (err) in
