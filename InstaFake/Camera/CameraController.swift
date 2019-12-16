83b8d97206be094817e6009565366870bc4f32eb
Aleksey Shapoval <alex@negativeimprint.com>
2019-12-16 14:32:00 -0500

    setup inputs for camera session

--- InstaFake/Camera/CameraController.swift
+++ InstaFake/Camera/CameraController.swift
@@ -7,9 +7,32 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupCaptureSession()
     }
+    
+    fileprivate func setupCaptureSession() {
+        let captureSession = AVCaptureSession()
+        
+        //1. setup inputs
+        guard let captureDevice = AVCaptureDevice.default(for: AVMediaType.video) else { return }
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            if captureSession.canAddInput(input) {
+                captureSession.addInput(input)
+            }
+        } catch let err {
+            print("Could not setup camera input:", err)
+        }
+        
+        //2. setup outputs
+        
+        //3. setup output preview
+    }
+    
 }
