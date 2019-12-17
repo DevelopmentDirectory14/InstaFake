aaae6a90a61bf5f65cbc008ba4f2c750ea475496
Aleksey Shapoval <alex@negativeimprint.com>
2019-12-17 15:55:44 -0500

    inserted cancel button into photo preview

--- InstaFake/Camera/PreviewPhotoContainerView.swift
+++ InstaFake/Camera/PreviewPhotoContainerView.swift
@@ -15,12 +15,26 @@ class PreviewPhotoContainerView: UIView {
         return iv
     }()
     
+    let cancelButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "cancel_shadow")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handleCancel), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc func handleCancel() {
+        self.removeFromSuperview()
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .yellow
         
         addSubview(previewImageView)
         previewImageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
+        addSubview(cancelButton)
+        cancelButton.anchor(top: topAnchor, left: leftAnchor, bottom: nil, right: nil, paddingTop: 12, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: 50, height: 50)
     }
     
     required init?(coder: NSCoder) {
