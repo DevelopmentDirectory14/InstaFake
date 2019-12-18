50fee131d0896b0a290839fc9623aef90f37d46e
Aleksey Shapoval <alex@negativeimprint.com>
2019-12-18 17:50:40 -0500

    edited text field so that the submit button can be clicked

--- InstaFake/Home/CommentsController.swift
+++ InstaFake/Home/CommentsController.swift
@@ -12,6 +12,8 @@ class CommentsController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.title = "Comments"
+        
         collectionView?.backgroundColor = .red
     }
     
@@ -25,24 +27,33 @@ class CommentsController: UICollectionViewController {
         tabBarController?.tabBar.isHidden = false
     }
     
+    var containerView: UIView = {
+        let containerView = UIView()
+        containerView.backgroundColor = .white
+        containerView.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        
+        let submitButton = UIButton(type: .system)
+        submitButton.setTitle("Submit", for: .normal)
+        submitButton.setTitleColor(.black, for: .normal)
+        submitButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        submitButton.addTarget(self, action: #selector(handleSubmit), for: .touchUpInside)
+        containerView.addSubview(submitButton)
+        submitButton.anchor(top: containerView.topAnchor, left: nil, bottom: containerView.bottomAnchor, right: containerView.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 12, width: 50, height: 0)
+        
+        let textField = UITextField()
+        textField.placeholder = "Enter Comment"
+        containerView.addSubview(textField)
+        textField.anchor(top: containerView.topAnchor, left: containerView.leftAnchor, bottom: containerView.bottomAnchor, right: submitButton.leftAnchor, paddingTop: 0, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
+        return containerView
+    }()
+    
+    @objc func handleSubmit() {
+        print("Handling submit...")
+    }
+    
     override var inputAccessoryView: UIView? {
         get {
-            let containerView = UIView()
-            containerView.backgroundColor = .white
-            containerView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 50)
-            
-            let submitButton = UIButton(type: .system)
-            submitButton.setTitle("Submit", for: .normal)
-            submitButton.setTitleColor(.black, for: .normal)
-            submitButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
-            containerView.addSubview(submitButton)
-            submitButton.anchor(top: containerView.topAnchor, left: nil, bottom: containerView.bottomAnchor, right: containerView.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 12, width: 50, height: 0)
-            
-            let textField = UITextField()
-            textField.placeholder = "Enter Comment"
-            containerView.addSubview(textField)
-            textField.anchor(top: containerView.topAnchor, left: containerView.leftAnchor, bottom: containerView.bottomAnchor, right: containerView.rightAnchor, paddingTop: 0, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
-            
             return containerView
         }
     }
