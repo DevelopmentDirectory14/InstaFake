cdb4f8607166fd623356f06a224b9ac8acaddde4
Aleksey Shapoval <alex@negativeimprint.com>
2019-12-18 17:17:04 -0500

    added submit button to comments controller

--- InstaFake/Home/CommentsController.swift
+++ InstaFake/Home/CommentsController.swift
@@ -31,10 +31,17 @@ class CommentsController: UICollectionViewController {
             containerView.backgroundColor = .white
             containerView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 50)
             
+            let submitButton = UIButton(type: .system)
+            submitButton.setTitle("Submit", for: .normal)
+            submitButton.setTitleColor(.black, for: .normal)
+            submitButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+            containerView.addSubview(submitButton)
+            submitButton.anchor(top: containerView.topAnchor, left: nil, bottom: containerView.bottomAnchor, right: containerView.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 12, width: 50, height: 0)
+            
             let textField = UITextField()
             textField.placeholder = "Enter Comment"
             containerView.addSubview(textField)
-            textField.anchor(top: containerView.topAnchor, left: containerView.leftAnchor, bottom: containerView.bottomAnchor, right: containerView.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+            textField.anchor(top: containerView.topAnchor, left: containerView.leftAnchor, bottom: containerView.bottomAnchor, right: containerView.rightAnchor, paddingTop: 0, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
             
             return containerView
         }
