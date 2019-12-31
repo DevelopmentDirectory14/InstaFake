2f4c12d8700e57fb5021b4dc9c7adb908ce9d9dd
Aleksey Shapoval <alex@negativeimprint.com>
2019-12-30 19:05:31 -0500

    display profile Controller posts in the correct reverse timestamp order

--- InstaFake/UserProfile/UserProfileController.swift
+++ InstaFake/UserProfile/UserProfileController.swift
@@ -42,6 +42,7 @@ class UserProfileController: UICollectionViewController, UICollectionViewDelegat
         //fetchOrderedPosts()
     }
     
+    var isFinishedPaging = false
     var posts = [Post]()
     
     fileprivate func paginatePosts() {
@@ -50,27 +51,31 @@ class UserProfileController: UICollectionViewController, UICollectionViewDelegat
         guard let uid = self.user?.uid else { return }
         let ref = Database.database().reference().child("posts").child(uid)
         
-//        let value = "-LxMZmsluPTdlcumgGzo"
-//        let query = ref.queryOrderedByKey().queryStarting(atValue: value).queryLimited(toFirst: 6)
-        
-        var query = ref.queryOrderedByKey()
+        var query = ref.queryOrdered(byChild: "creationDate")
         
         if posts.count > 0 {
-            let value = posts.last?.id
-            query = query.queryStarting(atValue: value)
+
+            let value = posts.last?.creationDate.timeIntervalSince1970
+            query = query.queryEnding(atValue: value)
         }
         
-        query.queryLimited(toFirst: 3).observeSingleEvent(of: .value, with: { (snapshot) in
+        query.queryLimited(toLast: 3).observeSingleEvent(of: .value, with: { (snapshot) in
             
-            var allObjects = snapshot.children.allObjects as? [DataSnapshot]
+            guard var allObjects = snapshot.children.allObjects as? [DataSnapshot] else { return }
             
-            if self.posts.count > 0 {
-                allObjects?.removeFirst()
+            allObjects.reverse()
+            
+            if allObjects.count < 3 {
+                self.isFinishedPaging = true
+            }
+            
+            if self.posts.count > 0 && allObjects.count > 0 {
+                allObjects.removeFirst()
             }
             
             guard let user = self.user else { return }
             
-            allObjects?.forEach({ (snapshot) in
+            allObjects.forEach({ (snapshot) in
                 
                 guard let dictionary = snapshot.value as? [String: Any] else { return }
                 var post = Post(user: user, dictionary: dictionary)
@@ -143,7 +148,7 @@ class UserProfileController: UICollectionViewController, UICollectionViewDelegat
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        if indexPath.item == self.posts.count - 1 {
+        if indexPath.item == self.posts.count - 1 && !isFinishedPaging {
             print("Paginating for posts")
             paginatePosts()
         }
